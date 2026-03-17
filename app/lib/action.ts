5b3ebc0cd317c384d15f99107e30efbfe172446e
Maninder Kaur <sainimaninder733@gmail.com>
2026-03-17 19:43:55 +0530

    Authentication Updates

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -107,7 +109,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
         await sql`
             DELETE FROM invoices
@@ -121,4 +122,23 @@ export async function deleteInvoice(id: string) {
             message: 'Database Error: Failed to Delete Invoice.',
         };
     }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
